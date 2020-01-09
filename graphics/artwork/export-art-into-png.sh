b69542593a9bb55047b6aa58dbf2079035e59b8c
Gabriel F <empresagabriel@gmail.com>
2020-01-09 05:58:56 -0300

    Changing some graphics (especially their palettes)

--- graphics/artwork/export-art-into-png.sh
+++ graphics/artwork/export-art-into-png.sh
@@ -3,5 +3,7 @@ for asefile in *.ase
 do
     pngfile="${asefile%.*}.png"
     echo "Exporting $asefile into $pngfile ..."
-    aseprite -b "$asefile" --save-as "$pngfile" &>>./export_log.log
+    aseprite -b "$asefile" --save-as "$pngfile" \
+             --palette /home/superwolf/aseprite/data/palettes/wp-gbc-tic-tac-toe-gbced.gpl\
+        &>>./export_log.log
 done
